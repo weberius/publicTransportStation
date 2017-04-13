d0d6eff7da70a9bcb9df82c903c5560039343c89
Wolfram Eberius <eberius@gmail.com>
2017-04-13 09:25:01 +0200

    implement StopsByLatLon #7

--- src/main/java/de/illilli/opendata/service/publicTransportStation/StopsByLatLonGeoJsonFacade.java
+++ src/main/java/de/illilli/opendata/service/publicTransportStation/StopsByLatLonGeoJsonFacade.java
@@ -1,19 +1,38 @@
 package de.illilli.opendata.service.publicTransportStation;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.NamingException;
+
+import org.geojson.Feature;
+import org.geojson.FeatureCollection;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import de.illilli.opendata.service.Facade;
+import de.illilli.opendata.service.publicTransportStation.jdbc.StopDTO;
+import de.illilli.opendata.service.publicTransportStation.model.Stop2GeoJson;
 
-public class StopsByLatLonGeoJsonFacade implements Facade {
+public class StopsByLatLonGeoJsonFacade extends StopsByLatLonFacade implements Facade {
 
-	public StopsByLatLonGeoJsonFacade(String latlng, String limit) {
-		// TODO Auto-generated constructor stub
+	private FeatureCollection featureCollection = new FeatureCollection();
+	private List<Feature> featureList = new ArrayList<Feature>();
+
+	public StopsByLatLonGeoJsonFacade(String latlng, String limit) throws SQLException, NamingException, IOException {
+		super(latlng, limit);
+		for (StopDTO dto : dtoList) {
+			featureList.add(new Stop2GeoJson(dto).getFeature());
+		}
+		featureCollection.addAll(featureList);
 	}
 
 	@Override
 	public String getJson() throws JsonProcessingException {
-		// TODO Auto-generated method stub
-		return null;
+		return new ObjectMapper().writeValueAsString(featureCollection);
 	}
 
 }
---
+++ src/test/java/de/illilli/opendata/service/publicTransportStation/StopsByLatLonGeoJsonFacadeTest.java
@@ -0,0 +1,38 @@
+package de.illilli.opendata.service.publicTransportStation;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.naming.NamingException;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import de.illilli.jdbc.ConnectionEnvironment;
+import de.illilli.opendata.service.Facade;
+
+public class StopsByLatLonGeoJsonFacadeTest {
+
+	private static final Logger logger = Logger.getLogger(StopsByLatLonGeoJsonFacadeTest.class);
+
+	@Before
+	public void setUp() throws Exception {
+		ConnectionEnvironment.setUpConnectionForJndi();
+	}
+
+	@Test
+	public void test() throws SQLException, NamingException, IOException {
+		String latlng = "50.96659064159747,6.9881160312996728";
+		String limitAsString = "5";
+		Facade facade = new StopsByLatLonGeoJsonFacade(latlng, limitAsString);
+		String expected = IOUtils
+				.toString(this.getClass().getResourceAsStream("/stopsByLatLonGeoJsonFacadeTest.geojson"));
+		String actual = facade.getJson();
+		logger.info(actual);
+		Assert.assertEquals(expected, actual);
+	}
+
+}
