62a547509e6977ef3da4a9d23ffdd34b7cc3eb6b
Wolfram Eberius <eberius@gmail.com>
2017-04-13 07:58:03 +0200

    implement StopsByBoundigBox #8

---
+++ src/main/java/de/illilli/opendata/service/publicTransportStation/StopsByBoundigBoxFacade.java
@@ -0,0 +1,18 @@
+package de.illilli.opendata.service.publicTransportStation;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import de.illilli.opendata.service.Facade;
+
+public class StopsByBoundigBoxFacade implements Facade {
+
+	public StopsByBoundigBoxFacade(String bbox) {
+	}
+
+	@Override
+	public String getJson() throws JsonProcessingException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
