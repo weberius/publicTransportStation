07947217eecd6d3865a5fd71b410da75314a8590
Wolfram Eberius <eberius@gmail.com>
2017-04-13 07:58:33 +0200

    implement StopsByLatLon #7

---
+++ src/main/java/de/illilli/opendata/service/publicTransportStation/StopsByLatLonFacade.java
@@ -0,0 +1,19 @@
+package de.illilli.opendata.service.publicTransportStation;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import de.illilli.opendata.service.Facade;
+
+public class StopsByLatLonFacade implements Facade {
+
+	public StopsByLatLonFacade(String latlng, String limit) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getJson() throws JsonProcessingException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/de/illilli/opendata/service/publicTransportStation/StopsByLatLonGeoJsonFacade.java
@@ -0,0 +1,19 @@
+package de.illilli.opendata.service.publicTransportStation;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import de.illilli.opendata.service.Facade;
+
+public class StopsByLatLonGeoJsonFacade implements Facade {
+
+	public StopsByLatLonGeoJsonFacade(String latlng, String limit) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getJson() throws JsonProcessingException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
